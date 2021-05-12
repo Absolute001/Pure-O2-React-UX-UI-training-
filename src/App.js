8bb47e4600b6316370367c8d5e2580ccc22866d3
Absolute001 <kshallvari@gmail.com>
2021-05-12 12:40:14 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
